bbd96eebb1ebadb1cff14bc43d236a6ea193ad92
Mykhailo Forkosh <forkos277@gmail.com>
2025-09-01 17:37:25 +0300

    fix input test

--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -9,7 +9,7 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  if (Number.isNaN(Number(userInput)) || userInput.length !== 4) {
+  if (!/^\d{4}$/.test(userInput)) {
     return false;
   }
 
